df24a338b906dd017b455d7243586580226a023d
Doghri.yassine@gmail.com <Doghri.yassine@gmail.com@824f7e52-6827-8676-89ab-1fe059d53f89>
2013-11-18 09:02:09 +0000

    Prise en compte des majuscules et minuscules pour la recherche par nom. Création du diagramme de classes.

--- gsbMedecins_web/src/java/model/Dep.java
+++ gsbMedecins_web/src/java/model/Dep.java
@@ -11,19 +11,20 @@ import java.util.HashSet;
  *
  * @author ydoghri
  */
-public class Dep implements Comparable<Dep>{
+public class Dep implements Comparable<Dep> {
+
     private String num;
-    private Collection <Med> lesMeds;
+    private Collection<Med> lesMeds;
 
     public Dep(String num, Collection<Med> lesMedecins) {
         this.num = num;
         this.lesMeds = lesMedecins;
     }
-    
+
     public String getNum() {
         return num;
     }
-    
+
     public int compareTo(Dep d) {
         return num.compareTo(d.num);
     }
@@ -31,15 +32,20 @@ public class Dep implements Comparable<Dep>{
     public Collection<Med> getLesMeds() {
         return lesMeds;
     }
-    
+
     public Collection getLesMedsR(String med) {
         Collection<Med> medSearch = new HashSet<Med>();
-        for(Med unMed : lesMeds) {
-            if (unMed.getNom().startsWith(med)) {
+        for (Med unMed : lesMeds) {
+            if (unMed.getNom().startsWith(firstLetterCaps(med))) {
                 medSearch.add(unMed);
             }
         }
         return medSearch;
     }
 
+    public String firstLetterCaps(String data) {
+        String firstLetter = data.substring(0, 1).toUpperCase();
+        String restLetters = data.substring(1).toLowerCase();
+        return firstLetter + restLetters;
+    }
 }
